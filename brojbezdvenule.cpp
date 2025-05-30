a16c720ec1beb72a4b438c970f776c2d9be31199
nikola <nikola181208@gmail.com>
2025-05-30 12:43:51 +0200

    Update brojbezdvenule.cpp

--- brojbezdvenule.cpp
+++ brojbezdvenule.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 
-void generisi_(unsigned tekuci, int preostaloCifara) {
+void generisi_(unsigned trenutni, int preostaloCifara) {
   
   // Bazični slučaj: ako nema više cifara za dodavanje
   if (preostaloCifara == 0) {
-    cout << tekuci << endl;  // ispiši broj
+    cout << trenutni << endl;  // ispiši broj
     return;
   }
 
   // Ako je poslednji bit trenutnog broja 1
-  if ((tekuci & 1) != 0)
+  if ((trenutni & 1) != 0)
     // Možeš da dodaš 0 na kraj broja
     // (pomeri sve bitove ulevo za jedno mesto — dodaj 0 na kraj)
-    generisi_(tekuci << 1, preostaloCifara - 1);
+    generisi_(trenutni << 1, preostaloCifara - 1);
   
   // Pomeri sve bitove ulevo i postavi poslednji bit na 1 pomoću | 1
-  generisi_((tekuci << 1) | 1, preostaloCifara - 1);
+  generisi_((trenutni << 1) | 1, preostaloCifara - 1);
 }
 
 // Funkcija koja pokreće generisanje binarnih brojeva dužine od 1 do n
